de2b88be5525a13ba6b679dd9cf6b1bfccf5cbd3
MuriloFerreir4 <159018937+MuriloFerreir4@users.noreply.github.com>
2024-02-05 10:03:13 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "iniciando instalações"
+ sudo apt update
+echo "updated"
+ sudo apt upgrade
+echo "upgraded"
+ sudo apt install mysql-server
+echo "mysql-server installed"
+ sudo apt install php
+echo "php installed"
+ sudo apt install  phpmyadmin
+echo "phpmyadmin installed"
+ sudp apt install apache2
+echo "apache2 installed"
+ sudo apt install ssh
+echo "ssh installed"
+echo"instalações finalizadas"
